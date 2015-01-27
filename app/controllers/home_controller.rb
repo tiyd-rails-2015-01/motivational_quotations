c586e5a8314c1feeb5dcead4392faf7527db6ddb
Chris Foster <fosterchris22@gmail.com>
2015-01-27 13:40:07 -0500

    Try out a random image selector.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
   def quotes
+    @moods = ["Villainous", "Dalai-Lama-Esque", "Melancholy", "Sanctimonious", "Coooool... B)"]
+    @image_folder = "#{params[:mood]}"
   end
 end
