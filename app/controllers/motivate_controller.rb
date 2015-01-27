f3c9fed63791086c927f4e696256aab63eb6b4d1
Rocky Randalls <rrandalls@gmail.com>
2015-01-27 18:48:20 -0500

    Clean up urls un code

--- app/controllers/motivate_controller.rb
+++ app/controllers/motivate_controller.rb
@@ -6,12 +6,14 @@ class MotivateController < ApplicationController
     elsif @feeling == 'Angry'
       @quote = 'Good, Goooood, let the hate flow through you!
       It gives you FOCUS!'
-      @image = 'http://cdn.screenrant.com/wp-content/uploads/Star-Wars-7-Rumor-Emperor-Returning.jpg'
+      @image = 'http://cdn.screenrant.com/wp-content/uploads/'\
+               'Star-Wars-7-Rumor-Emperor-Returning.jpg'
     elsif @feeling == 'Happy'
       @quote = 'Believe in yourself! Have faith in your abilities!
       Without a humble but reasonable confidence in your own powers
       you cannot be successful or happy. Also, Nyan cat!'
-      @image = 'http://fc04.deviantart.net/fs70/f/2011/107/c/e/rainbow_nyan_nyan_pop_tart_cat_by_zaithy-d3e8u2k.jpg'
+      @image = 'http://fc04.deviantart.net/fs70/f/2011/107/'\
+               'c/e/rainbow_nyan_nyan_pop_tart_cat_by_zaithy-d3e8u2k.jpg'
     elsif @feeling == 'Meh'
       @quote = 'The trouble with having an open mind, of course,
       is that people will insist on coming along and trying to
