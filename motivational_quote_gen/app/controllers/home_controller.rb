2b96bc47be220d4d173ebccbdbf222356c65bb4a
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-01-27 15:47:33 -0500

    Adds random pictures for some moods, not fully implemented

--- motivational_quote_gen/app/controllers/home_controller.rb
+++ motivational_quote_gen/app/controllers/home_controller.rb
@@ -3,10 +3,13 @@ class HomeController < ApplicationController
     @mood = params[:input_mood]
     if @mood == "Happy"
       @quote = happy_quotes
+      @picture = happy_picture
     elsif @mood == "Blah"
       @quote = blah_quotes
+      @picture = blah_picture
     elsif @mood == "Sad"
       @quote = sad_quotes
+      @picture = sad_picture
     elsif @mood == "Morose"
       @quote = morose_quotes
     end
@@ -39,9 +42,40 @@ class HomeController < ApplicationController
   def morose_quotes
     morose_quotes = {1=>"Stop feeling sorry for yourself!",
                      2=>"Life getting you down?",
-                     3=>"YOLO, tale control of it",
+                     3=>"YOLO, take control of it",
                      4=>"Not sure what morose means exactly lol",}
     return morose_quotes[rand(1..4)]
   end
+
+  def happy_picture
+    happy_link = {1=>"https://i1.sndcdn.com/artworks-000074067288-cpc6s6-t500x500.jpg",
+                  2=>"http://www.deliveringhappiness.com/wp-content/uploads/2011/10/happyball.jpg",
+                  3=>"http://blog.hillsbiblechurch.org/wp-content/uploads/2012/03/Self-satisfied.png",
+                  4=>"http://bit.ly/1LfcStD",
+                  }
+    return happy_link[rand(1..4)]
+  end
+
+  def sad_picture
+    sad_link = {1=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHSlJRYxaIAkHxLosUXfpQbPppOB7wHBA4SRjnWCTrpUryJ4N7nw",
+                2=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSu6E9M2FcCSkM1qiioWhbKfw4KRjkPvIDvvwCOXwrVGJyLjP3Z",
+                3=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8aayatwhmdloi1bjiny7kL83Mq7NY9oQairayKdTHdc7dN94_og",
+                4=>"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSjgp6_goE8T3j4tcSjwbYSrg_LcsBdnIDF3HVEiz8gHVqgfLeObQ",
+                }
+    return sad_link[rand(1..4)]
+  end
+
+  def blah_picture
+    blah_link = {1=>"",
+                 2=>"",
+                 3=>"",
+                 4=>"",
+                 }
+    return blah_link[rand(1..4)]
+  end
+
+  def morose_picture
+  end
+
 
 end
