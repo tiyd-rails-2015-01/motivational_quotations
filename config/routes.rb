7f6ea71bdff2cc60ce916e8731c5d1074b2c84c5
Jonathan Parrish <jonathan.s.parrish@gmail.com>
2015-01-27 18:25:51 -0500

    First round of code that works

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  get 'home/login'
+
 
   get 'home/dashboard'
+  post 'home/dashboard'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#dashboard'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
