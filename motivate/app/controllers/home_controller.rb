99f6b68b3d11f7a113ebe0863adfe94cf2f35a47
C Haden <chaden@spoonflower.com>
2015-01-27 17:37:16 -0500

    minor fiddling

--- motivate/app/controllers/home_controller.rb
+++ motivate/app/controllers/home_controller.rb
@@ -1,4 +1,43 @@
 class HomeController < ApplicationController
   def yourmood
+    @mood = params[:mood]
+
+
+    if @mood == "Chipper"
+      @motivation = [ "Hello sunshine!",
+                      "Whee!",
+                      "Well, hello!"]
+      @goat_src = "/assets/chipper_goat.jpg"
+
+    elsif @mood == "Grouchy"
+      @motivation = ["Helloooooo sunshine!"]
+      @goat_src = "/assets/grouchy_goat.jpg"
+
+    elsif @mood == "Punchy"
+      @motivation = ["Hey, You should try taking this test 500 times for a special surprise!"]
+      @goat_src = "/assets/punchy_goat.jpg"
+
+    elsif @mood == "Fell"
+      @motivation = ["DOOM! DOOOOOOOOOM!"]
+      @goat_src = "/assets/doom_goat.jpg"
+
+    elsif @mood == "Exultant"
+      @motivation = ["Success!"]
+      @goat_src = "/assets/exult_goat.jpg"
+
+    elsif @mood == "Vengeful"
+      @motivation = ["You are swift and merciless!"]
+      @goat_src = "/assets/vengeful_goat.jpg"
+
+    elsif @mood == "Bourgeois"
+      @motivation = ["They tell me greed is good!"]
+      @goat_src = "/assets/rich_goat.jpg"
+
+    end
+    if( @motivation != nil )
+      @returned_val = @motivation.sample
+    end
+
   end
+
 end
