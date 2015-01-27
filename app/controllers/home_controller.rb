27cc8e1af63c484103ae25f5b7eda102510692e9
Chris Foster <fosterchris22@gmail.com>
2015-01-27 16:26:57 -0500

    Image selector works. Upload images.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def quotes
-    @moods = ["Villainous", "Dalai-Lama-Esque", "Melancholy", "Sanctimonious", "Coooool... B)"]
-    @image_folder = "#{params[:mood]}"
+    @moods = ["Villainous", "Dalai-Lama-Esque", "Melancholy", "Sanctimonious", "Coooool"]
+
+    @image_folder = params[:mood]
+    @quote =
   end
 end
