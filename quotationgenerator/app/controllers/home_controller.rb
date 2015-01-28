848457a74b53d819795ec23117832af105185d0c
Ruben <ssrcruz@gmail.com>
2015-01-27 20:25:56 -0500

    Last Commit.Basic Quotation Generator.

--- quotationgenerator/app/controllers/home_controller.rb
+++ quotationgenerator/app/controllers/home_controller.rb
@@ -24,6 +24,12 @@ class HomeController < ApplicationController
       " you know that you cannot. Running away from fear is fear, fighting pain is pain, trying to be"+
       " brave is being scared. If the mind is in pain, the mind is pain. The thinker has no other form than his thought. There is no escape. - Alan Watts"
       @mood_image = "http://upload.wikimedia.org/wikipedia/en/b/b1/Alan_Watts.jpg"
+    elsif @mood == "Peaceful"
+      @quote = "Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment. - Buddha"
+      @mood_image = "http://www.johnworldpeace.com/images/buddha017.jpg"
+    elsif @mood == "Cheerful"
+      @quote = "I love those who can smile in trouble... - Leonardo Da Vinci"
+      @mood_image = "http://d.gr-assets.com/authors/1399080979p8/13560.jpg"
     end
 
   end
