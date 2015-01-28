e571d1027721b95140c412874afd2fb273c0e93f
Rocky Randalls <rrandalls@gmail.com>
2015-01-28 10:42:29 -0500

    Clean up code, add model

--- app/controllers/motivate_controller.rb
+++ app/controllers/motivate_controller.rb
@@ -6,23 +6,21 @@ class MotivateController < ApplicationController
     elsif @feeling == 'Angry'
       @quote = 'Good, Goooood, let the hate flow through you!
       It gives you FOCUS!'
-      @image = 'http://cdn.screenrant.com/wp-content/uploads/'\
-               'Star-Wars-7-Rumor-Emperor-Returning.jpg'
+      @image = 'http://goo.gl/HLnfya'
     elsif @feeling == 'Happy'
       @quote = 'Believe in yourself! Have faith in your abilities!
       Without a humble but reasonable confidence in your own powers
       you cannot be successful or happy. Also, Nyan cat!'
-      @image = 'http://fc04.deviantart.net/fs70/f/2011/107/'\
-               'c/e/rainbow_nyan_nyan_pop_tart_cat_by_zaithy-d3e8u2k.jpg'
+      @image = 'http://goo.gl/D0gTje'
     elsif @feeling == 'Meh'
       @quote = 'The trouble with having an open mind, of course,
       is that people will insist on coming along and trying to
       put things in it.'
-      @image = 'http://embodyheart.org/wp-content/uploads/2013/03/meh.jpg'
+      @image = 'http://goo.gl/eCQb9a'
     elsif @feeling == 'Motivated'
       @quote = 'In order to succeed, your desire for success should
       be greater than your fear of failure.'
-      @image = 'http://www.entre-propel.com/files/2009/01/motivation.jpeg'
+      @image = 'http://goo.gl/nFiepe'
     end
   end
 end
